d106d92d9c20baee53ad503cc41264251b970be7
xuruoxuan <ruoxuan1995@gmail.com>
2019-08-22 16:09:13 -0400

    spring initializer code

---
+++ src/main/java/moe/mkx/uimf/groupbuilder/GroupbuilderApplication.java
@@ -0,0 +1,13 @@
+package moe.mkx.uimf.groupbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupbuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/mkx/uimf/groupbuilder/GroupbuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package moe.mkx.uimf.groupbuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupbuilderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
